9e76d97fe5c926e6910b05a6f67dd5b10ef8b270
nguyen.van.huynh.y <nguyen.van.huynh.y@styl.solutions>
2021-08-12 14:41:11 +0700

    add u-boot

---
+++ board/variscite/imx8m_var_dart/imx8m_var_dart.c
@@ -0,0 +1,341 @@
+/*
+ * Copyright 2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
+ * Copyright 2018-2019 Variscite Ltd.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <miiphy.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/imx8mq_pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <asm/arch/clock.h>
+#include <asm/mach-imx/video.h>
+#include <asm/arch/video_common.h>
+#include <splash.h>
+
+#include "../common/imx8m_eeprom.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_early_init_f(void)
+{
+	return 0;
+}
+
+#ifdef CONFIG_BOARD_POSTCLK_INIT
+int board_postclk_init(void)
+{
+	/* TODO */
+	return 0;
+}
+#endif
+
+/* Return DRAM size in bytes */
+static unsigned long long get_dram_size(void)
+{
+	int ret;
+	u32 dram_size_mb;
+	struct var_eeprom eeprom;
+
+	var_eeprom_read_header(&eeprom);
+	ret = var_eeprom_get_dram_size(&eeprom, &dram_size_mb);
+	if (ret)
+		return (1ULL * DEFAULT_DRAM_SIZE_MB ) << 20;
+
+	return (1ULL * dram_size_mb) << 20;
+}
+
+int dram_init_banksize(void)
+{
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size = get_dram_size();
+
+	return 0;
+}
+
+int dram_init(void)
+{
+	unsigned long long mem_size, max_low_size, dram_size;
+
+	max_low_size = 0x100000000ULL - CONFIG_SYS_SDRAM_BASE;
+	dram_size = get_dram_size();
+	
+	if (dram_size > max_low_size)
+		mem_size = max_low_size;
+	else
+		mem_size = dram_size;
+
+	/* rom_pointer[1] contains the size of TEE occupies */
+	gd->ram_size = mem_size - rom_pointer[1];
+
+	return 0;
+}
+
+#ifdef CONFIG_OF_BOARD_SETUP
+int ft_board_setup(void *blob, bd_t *bd)
+{
+	return 0;
+}
+#endif
+
+#ifdef CONFIG_FEC_MXC
+static iomux_v3_cfg_t const fec_pads[] = {
+	IMX8MQ_PAD_GPIO1_IO08__GPIO1_IO8 | MUX_PAD_CTRL(NO_PAD_CTRL),
+	IMX8MQ_PAD_GPIO1_IO09__GPIO1_IO9 | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static int setup_mac(struct var_eeprom *eeprom)
+{
+	int ret;
+	unsigned char enetaddr[6];
+
+	ret = eth_env_get_enetaddr("ethaddr", enetaddr);
+	if (ret)
+		return 0;
+
+	ret = var_eeprom_get_mac(eeprom, enetaddr);
+	if (ret)
+		return ret;
+
+	if (!is_valid_ethaddr(enetaddr))
+		return -1;
+
+	return eth_env_set_enetaddr("ethaddr", enetaddr);
+}
+
+static void setup_iomux_fec(void)
+{
+	imx_iomux_v3_setup_multiple_pads(fec_pads, ARRAY_SIZE(fec_pads));
+
+	/* Power-up ethernet PHY */
+	gpio_request(IMX_GPIO_NR(1, 8), "phy_pwr");
+	gpio_direction_output(IMX_GPIO_NR(1, 8), 1);
+	mdelay(10);
+
+	/* Reset ethernet PHY */
+	gpio_request(IMX_GPIO_NR(1, 9), "phy_rst");
+	gpio_direction_output(IMX_GPIO_NR(1, 9), 0);
+	mdelay(10);
+	gpio_direction_output(IMX_GPIO_NR(1, 9), 1);
+}
+
+static int setup_fec(void)
+{
+	struct iomuxc_gpr_base_regs *const iomuxc_gpr_regs
+		= (struct iomuxc_gpr_base_regs *) IOMUXC_GPR_BASE_ADDR;
+
+	setup_iomux_fec();
+
+	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
+	clrsetbits_le32(&iomuxc_gpr_regs->gpr[1],
+			IOMUXC_GPR_GPR1_GPR_ENET1_TX_CLK_SEL_SHIFT, 0);
+
+	return set_clk_enet(ENET_125MHZ);
+}
+
+#define AR803x_PHY_DEBUG_ADDR_REG	0x1d
+#define AR803x_PHY_DEBUG_DATA_REG	0x1e
+
+#define AR803x_DEBUG_REG_5		0x05
+#define AR803x_DEBUG_REG_0		0x00
+
+int board_phy_config(struct phy_device *phydev)
+{
+	/* Disable RGMII RX clock delay (enabled by default) */
+	phy_write(phydev, MDIO_DEVAD_NONE, AR803x_PHY_DEBUG_ADDR_REG,
+		  AR803x_DEBUG_REG_0);
+	phy_write(phydev, MDIO_DEVAD_NONE, AR803x_PHY_DEBUG_DATA_REG, 0);
+
+	if (phydev->drv->config)
+		phydev->drv->config(phydev);
+
+	return 0;
+}
+#endif
+
+int board_init(void)
+{
+#ifdef CONFIG_FEC_MXC
+	setup_fec();
+#endif
+
+#if defined(CONFIG_USB_DWC3) || defined(CONFIG_USB_XHCI_IMX8M)
+	init_usb_clk();
+#endif
+
+	return 0;
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return devno;
+}
+
+int mmc_map_to_kernel_blk(int dev_no)
+{
+	return dev_no;
+}
+
+#define SDRAM_SIZE_STR_LEN 5
+int board_late_init(void)
+{
+	struct var_eeprom eeprom;
+	char sdram_size_str[SDRAM_SIZE_STR_LEN];
+
+	var_eeprom_read_header(&eeprom);
+
+#ifdef CONFIG_FEC_MXC
+	setup_mac(&eeprom);
+#endif
+	var_eeprom_print_prod_info(&eeprom);
+
+#ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+	env_set("board_name", "IMX8M-VAR-DART");
+	env_set("board_rev", "iMX8MQ");
+#endif
+
+	snprintf(sdram_size_str, SDRAM_SIZE_STR_LEN, "%d", (int) (gd->ram_size / 1024 / 1024));
+	env_set("sdram_size", sdram_size_str);
+
+#ifdef CONFIG_ENV_IS_IN_MMC
+	board_late_mmc_env_init();
+#endif
+
+	return 0;
+}
+
+#ifdef CONFIG_FSL_FASTBOOT
+#ifdef CONFIG_ANDROID_RECOVERY
+
+#define BACK_KEY IMX_GPIO_NR(4, 6)
+#define BACK_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE)
+
+static iomux_v3_cfg_t const back_pads[] = {
+	IMX8MQ_PAD_SAI1_RXD4__GPIO4_IO6 | MUX_PAD_CTRL(BACK_PAD_CTRL),
+};
+
+int is_recovery_key_pressing(void)
+{
+	imx_iomux_v3_setup_multiple_pads(back_pads, ARRAY_SIZE(back_pads));
+	gpio_request(BACK_KEY, "BACK");
+	gpio_direction_input(BACK_KEY);
+	if (gpio_get_value(BACK_KEY) == 0) { /* BACK key is low assert */
+		printf("Recovery key pressed\n");
+		return 1;
+	}
+	return 0;
+}
+#endif /*CONFIG_ANDROID_RECOVERY*/
+#endif /*CONFIG_FSL_FASTBOOT*/
+
+#if defined(CONFIG_VIDEO_IMXDCSS)
+
+struct display_info_t const displays[] = {{
+	.bus	= 0, /* Unused */
+	.addr	= 0, /* Unused */
+	.pixfmt	= GDF_32BIT_X888RGB,
+	.detect	= NULL,
+	.enable	= NULL,
+#ifndef CONFIG_VIDEO_IMXDCSS_1080P
+	.mode	= {
+		.name           = "HDMI", /* 720P60 */
+		.refresh        = 60,
+		.xres           = 1280,
+		.yres           = 720,
+		.pixclock       = 13468, /* 74250  kHz */
+		.left_margin    = 110,
+		.right_margin   = 220,
+		.upper_margin   = 5,
+		.lower_margin   = 20,
+		.hsync_len      = 40,
+		.vsync_len      = 5,
+		.sync           = FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
+		.vmode          = FB_VMODE_NONINTERLACED
+	}
+#else
+	.mode	= {
+		.name           = "HDMI", /* 1080P60 */
+		.refresh        = 60,
+		.xres           = 1920,
+		.yres           = 1080,
+		.pixclock       = 6734, /* 148500 kHz */
+		.left_margin    = 148,
+		.right_margin   = 88,
+		.upper_margin   = 36,
+		.lower_margin   = 4,
+		.hsync_len      = 44,
+		.vsync_len      = 5,
+		.sync           = FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
+		.vmode          = FB_VMODE_NONINTERLACED
+	}
+#endif
+} };
+size_t display_count = ARRAY_SIZE(displays);
+
+#endif /* CONFIG_VIDEO_IMXDCSS */
+
+#ifdef CONFIG_SPLASH_SCREEN
+
+static int check_env(char *var, char *val)
+{
+	char *env_val = env_get(var);
+
+	if ((env_val != NULL) &&
+		(strcmp(env_val, val) == 0)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+static void splash_set_source(void)
+{
+	if (!check_env("splashsourceauto", "yes"))
+		return;
+
+	if (mmc_get_env_dev() == 0)
+		env_set("splashsource", "emmc");
+	else if (mmc_get_env_dev() == 1)
+		env_set("splashsource", "sd");
+}
+
+int splash_screen_prepare(void)
+{
+	char sd_devpart[5];
+	char emmc_devpart[5];
+	u32 sd_part, emmc_part;
+
+	sd_part = emmc_part = env_get_ulong("mmcpart", 10, 0);
+
+	sprintf(sd_devpart, "1:%d", sd_part);
+	sprintf(emmc_devpart, "0:%d", emmc_part);
+
+	struct splash_location splash_locations[] = {
+		{
+			.name = "sd",
+			.storage = SPLASH_STORAGE_MMC,
+			.flags = SPLASH_STORAGE_FS,
+			.devpart = sd_devpart,
+		},
+		{
+			.name = "emmc",
+			.storage = SPLASH_STORAGE_MMC,
+			.flags = SPLASH_STORAGE_FS,
+			.devpart = emmc_devpart,
+		}
+	};
+
+	splash_set_source();
+
+	return splash_source_load(splash_locations,
+			ARRAY_SIZE(splash_locations));
+
+}
+#endif /* CONFIG_SPLASH_SCREEN */
